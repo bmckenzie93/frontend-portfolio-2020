0b76415ee0fa13af9db4acdff640094534d08162
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2020-12-31 18:58:35 -0800

    about paragraph added

--- src/components/layout/About/About.js
+++ src/components/layout/About/About.js
@@ -7,22 +7,31 @@ import Heading from '../../UI/Heading/Heading'
 const About = () => {
   return (
     <section className={styles.About}>
-      <Heading title ='About' />
+      <Heading title ='about' />
+
+      <h1 style={{ fontWeight: 'lighter', marginBottom: '3rem' }}>
+        I love to design and code responsive websites and web applications.
+        <br/><a href="#">Let's make something special.</a>
+      </h1>
 
       <Flex>
         <Card>
           <div className="card-icon">
             <div className="fas fa-mobile-alt fa-4x"></div>
           </div>
-
-            <h3>Responsive Layouts</h3>
+          <div className="card-text">
+            <h3 style={{marginBottom: '1rem', color: '#e31b6d' }}>Responsive Layouts</h3>
             <p>My designs will work on any device including desktops, tablets, and mobile phones.</p>
+          </div>
+
         </Card>
 
         <Card>
           <div className="card-icon">
             <div className="fas fa-code fa-4x"></div>
-            <h3>Dynamic Functionality</h3>
+          </div>
+          <div className="card-text">
+            <h3 style={{marginBottom: '1rem', color: '#e31b6d' }}>Dynamic Functionality</h3>
             <p>I make pages come to life with slideshows, popups, animations and more.</p>
           </div>
         </Card>
@@ -30,8 +39,10 @@ const About = () => {
         <Card>
           <div className="card-icon">
             <div className="fas fa-rocket fa-4x"></div>
-            <h3>Deployment & Management</h3>
-            <p>I can get your website onto the internet and connect you world wide!</p>
+          </div>
+          <div className="card-text">
+            <h3 style={{marginBottom: '1rem', color: '#e31b6d' }}>Deployment & Management</h3>
+            <p>I can get your website onto the internet and in front of the entire world.</p>
           </div>
         </Card>
       </Flex>
