87c3ec2095139f92b02e714062e060ecd407f0b6
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2021-01-05 13:03:59 -0800

    projects trimmed up

--- src/components/layout/Projects/Projects.js
+++ src/components/layout/Projects/Projects.js
@@ -5,8 +5,6 @@ import styles from './Projects.module.css'
 
 import immersed_in_art from '../../../assets/immersed_in_art.png'
 import wild_water_world from '../../../assets/wild_water_world.png'
-import poke_stats from '../../../assets/poke_stats.png'
-import street_smart_developer from '../../../assets/street_smart_developer.png'
 import Container from '../../UI/Container/Container'
 
 
@@ -20,20 +18,13 @@ const Projects = () => {
           color="#000" />
 
         <Project 
-          title='Immersed in Art'
-          image={immersed_in_art} />
-
-        <Project 
           title='Wild Water World'
           image={wild_water_world} />
 
         <Project 
-          title="Poke' Stats"
-          image={poke_stats} />
-          
-        <Project 
-          title='Street Smart Developer'
-          image={street_smart_developer} />
+          title='Immersed in Art'
+          image={immersed_in_art} />
+
       </Container>
     </section>
   )
