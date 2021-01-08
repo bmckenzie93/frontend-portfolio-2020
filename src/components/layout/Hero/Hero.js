46ad423484cffb22f97af00161ae0ef1f64f9421
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2021-01-08 13:27:32 -0800

    gsap installed, hero animated

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import styles from './Hero.module.css'
-import A from '../../UI/Button/Link'
 import Background from '../../../assets/digitalBG.mp4'
 import Container from '../../UI/Container/Container'
+
 import { Link } from 'react-scroll'
+import { TweenMax, Power3 } from 'gsap'
 
 
 const Hero = () => {
+  let p1 = useRef(null);
+  let p2 = useRef(null);
+  let cta = useRef(null);
+
+  useEffect(() => {
+    // P1
+    TweenMax.from(
+      p1,
+      1.5,
+      {
+        opacity: 0,
+        x: -60,
+        ease: Power3.easeOut
+      }
+    )
+
+    // P2
+    TweenMax.from(
+      p2,
+      1.5,
+      {
+        opacity: 0,
+        x: 60,
+        ease: Power3.easeOut,
+        delay: .3
+      }
+    )
+
+    // CTA
+    TweenMax.from(
+      cta,
+      1.5,
+      {
+        opacity: 0,
+        y: 60,
+        ease: Power3.easeOut,
+        delay: .5
+      }
+    )
+  }, [])
+
   return (
     <header 
       className={styles.Hero}
@@ -17,10 +59,16 @@ const Hero = () => {
       </video>
       
       <Container size="Large">
-        <p>Hello, I'm <b style={{color: '#e31b6d'}}>Brandon McKenzie</b>.</p>
-        <p>I'm a front-end web developer.</p>
+        <p ref={el => { p1 = el }}>
+          Hello, I'm <b style={{color: '#e31b6d'}}>Brandon McKenzie</b>.
+        </p>
+        <p ref={el => { p2 = el }}>
+          I'm a front-end web developer.
+        </p>
 
-        <div className={styles.LinkDiv}>
+        <div 
+          className={styles.LinkDiv}
+          ref={el => { cta = el }} >
           <Link
             className={styles.Button}
             to='about'
